8e67e69882e80b1cfd44bbe802b5f8202fb18da5
Sandy Fu <fusandy8260@gmail.com>
2022-01-21 15:06:32 +0800

    node.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+
+app.use(cors())
+
+app.get('/', cors(), function (req, res) {
+  res.send('Hello World')
+})
+
+
+console.log("server is on: http://localhost:3000/")
+app.listen(3000)
