1732fdf92d55f7e5d9453573d84b55500fd7704e
Sandy Fu <fusandy8260@gmail.com>
2022-01-24 17:54:28 +0800

    test

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', cors(), function (req, res) {
+app.post('/', cors(), function (req, res) {
   res.send('Hello World')
 })
+
+app.get('/', cors(), function (req, res) {
+  res.send('Hello World')
+});
+
 
 
 console.log("server is on: http://localhost:3000/")
