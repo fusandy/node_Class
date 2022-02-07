d039f6d2ec0da4582276f6bf64c3ad4c58dc3bda
Sandy Fu <fusandy8260@gmail.com>
2022-02-07 15:46:34 +0800

    0207 URL originalURL baseURL

--- routes/admin2.js
+++ routes/admin2.js
@@ -1,11 +1,28 @@
 // index.js主程式已經require('express')，這邊再require只會拿到參照
 const express = require('express');
 
-
+// 建立router物件
 const router = express.Router();
 
+router.get('/', (req, res)=>{
+    res.send('admin2: root');
+});
+
 router.get('/admin2/:p1?/:p2?', (req,res)=>{
-    res.json(req.params);
+    let {
+        params,
+        url,
+        originalUrl,
+        baseUrl,
+    } = req;
+
+    res.json({
+        params,
+        url,
+        originalUrl,
+        baseUrl,
+    });
+    // res.json(req.params);
 })
 
 module.exports = router;
